892c51e75db9135db4d972b3db16a97fb1d9eb84
maymine <sm9365277@gmail.com>
2023-08-26 14:43:45 -0500

    added routes actioncable

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
+
+  
   resources :messages
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
